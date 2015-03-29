19eb27c486f16d1129ccd10c8b16a2092699836c
unknown <mati0029@WJ316-20.ottawa.ad.algonquincollege.com>
2015-03-29 10:40:54 -0400

    difficulty changed

--- Trunk/BadTeeth/Assets/Scripts/Misc/Constants.cs
+++ Trunk/BadTeeth/Assets/Scripts/Misc/Constants.cs
@@ -5,7 +5,7 @@ public class Constants
 {
 	public const int MAX_STAMINA = 1000;
 
-	public const int POLICE_STAMINA_HIT = 460;
+	public const int POLICE_STAMINA_HIT = 450;
 
 	public const int JUMP_COST = 200;
 	public const int DOUBLE_JUMP_COST = 300;
@@ -14,7 +14,7 @@ public class Constants
     public const int RUN_COST = 1000;
 
 	public const float MAX_THREAT_LEVEL = 10.0f;
-	public const int POLICE_PER_THREAT_RATIO = 10;
+	public const int POLICE_PER_THREAT_RATIO = 6;
 
     public const float JUMP_THREAT = 0.03f;
     public const float DOUBLE_JUMP_THREAT = 0.1f;
