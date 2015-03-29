27059f83ae35be44b849fbed687dcf041a847cd0
unknown <mati0029@WJ316-20.ottawa.ad.algonquincollege.com>
2015-03-28 23:19:45 -0400

    input select added

---
+++ Trunk/BadTeeth/Assets/Scripts/Misc/InputSelect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputSelect : MonoBehaviour 
+{
+	public string m_nextLevel = "Game";
+	// Update is called once per frame
+	void Update () 
+	{
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            InputManager.IsUsingKeyboard = true;
+            Application.LoadLevel(m_nextLevel);
+        }
+
+        if(GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.Any))
+        {
+            InputManager.IsUsingKeyboard = false;
+            Application.LoadLevel(m_nextLevel);
+        }
+	}
+}
