9e4d682713e4e82b7d5235bb76b10873adbcc6f0
unknown <bell0442@WJ316-22.ottawa.ad.algonquincollege.com>
2015-03-29 09:39:22 -0400

    Added delay to being able to press any key to go back to splash

--- Trunk/BadTeeth/Assets/Scripts/Misc/PressAnyKeyToSplash.cs
+++ Trunk/BadTeeth/Assets/Scripts/Misc/PressAnyKeyToSplash.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class PressAnyKeyToSplash : MonoBehaviour 
 {	
+	float m_TimeBeforeInput = 2.0f;
+
 	// Update is called once per frame
 	void Update () 
 	{
-		if(InputManager.getJumpDown ())
+		m_TimeBeforeInput -= Time.deltaTime;
+
+		if(m_TimeBeforeInput < 0.0f && InputManager.getJumpDown ())
 		{
 			Application.LoadLevel("InputSelect");
 		}
