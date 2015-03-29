8968911d0ce65c83be10fdb473ccb0965cedb2cb
unknown <bell0442@WJ316-22.ottawa.ad.algonquincollege.com>
2015-03-29 10:10:09 -0400

    Added HUD for tags remaining

--- Trunk/BadTeeth/Assets/Scripts/Signs/Sign.cs
+++ Trunk/BadTeeth/Assets/Scripts/Signs/Sign.cs
@@ -80,7 +80,13 @@ public class Sign : MonoBehaviour
 
     LineRenderer m_LineRenderer;
     public Vector3 i_ProgressStart = new Vector3(-1.0f, 1.0f, 0.0f);
-    public Vector3 i_ProgressEnd = new Vector3(1.0f, 1.0f, 0.0f);
+	public Vector3 i_ProgressEnd = new Vector3(1.0f, 1.0f, 0.0f);
+	
+	const string STAMINA_TEXTURES_PATH = "Textures/Stamina";
+	Texture[] m_StaminaTextures;
+	
+	Vector2 i_StaminaBarPosition = new Vector2(0.004f, 0.01f);
+	Vector2 i_StaminaBarScale = new Vector2(0.30f, 0.03f);
 
 	// Use this for initialization
 	void Start () 
@@ -115,10 +121,24 @@ public class Sign : MonoBehaviour
 		m_LineRenderer.SetPosition(0, transform.position + i_ProgressStart);
 		
 		PaintedTime = m_PaintedTime;
+
+		m_StaminaTextures = Resources.LoadAll<Texture>(STAMINA_TEXTURES_PATH);
 	}
 
 	void updateLine()
 	{
         m_LineRenderer.SetPosition(1, (transform.position + i_ProgressStart) + ((i_ProgressEnd - i_ProgressStart) * (m_PaintedTime / TIME_TO_PAINT[(int)m_SignType])));
+	}
+
+	void OnGUI()
+	{
+		Color previousColor = GUI.color;
+		GUI.color = Color.red;
+
+		Rect StaminaBar = new Rect((1.0f - i_StaminaBarPosition.x) * Screen.width - (i_StaminaBarScale.x * Screen.width) * (1.0f - (float)taggedSigned / (float)numSigns), i_StaminaBarPosition.y * Screen.height,
+		                           (i_StaminaBarScale.x * Screen.width) * (1.0f - (float)taggedSigned / (float)numSigns), i_StaminaBarScale.y * Screen.height);
+		GUI.DrawTexture(StaminaBar, m_StaminaTextures[2]);
+
+		GUI.color = previousColor;
 	}
 }
