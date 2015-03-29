e9af692c1d8b1f3815d62d9f59a1c9ebc04ca9c2
unknown <mati0029@WJ316-20.ottawa.ad.algonquincollege.com>
2015-03-28 22:44:06 -0400

    run added

--- Trunk/BadTeeth/Assets/Scripts/Misc/Constants.cs
+++ Trunk/BadTeeth/Assets/Scripts/Misc/Constants.cs
@@ -9,6 +9,8 @@ public class Constants
 	public const int JUMP_COST = 200;
 	public const int DOUBLE_JUMP_COST = 300;
 
+    public const int RUN_COST = 1000;
+
 	public const float MAX_THREAT_LEVEL = 10.0f;
 	public const int POLICE_PER_THREAT_RATIO = 10;
 }
